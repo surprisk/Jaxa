cb92ec5e841d522312f4d59306970465d459f08c
Samuel <brosse.samuel91@gmail.com>
2024-01-09 16:58:22 +0100

    Jaxa main function surchage support

--- index.js
+++ index.js
@@ -3,16 +3,41 @@ import http from "http";
 
 const protocols = { https, http };
 
-export function Jaxa(url, callbacks = {}, options = {method: 'GET'}){
-    const item = this;
-    const protocol = new URL(url).protocol.slice(0, -1);
+export function Jaxa(){
+
+    let argumentsArray = Object.values(arguments)
+    let callbacks = {};
+    const protocol = typeof argumentsArray[0] == "string" ? 
+        new URL(argumentsArray[0]).protocol.slice(0, -1) :
+        argumentsArray[0].protocol.slice(0, -1);
+
+    switch(argumentsArray.length){
+        case 1: 
+            if(typeof argumentsArray[0] == "string")
+                argumentsArray.push({method: 'GET'})
+        break;
+        case 2:
+            const objectKeys = Object.keys(argumentsArray[1])
+            if(['data', 'end', 'error'].filter((key) => objectKeys.includes(key)).length > 0)
+                callbacks = argumentsArray.pop();
+        break;
+        case 3:
+            callbacks = argumentsArray.pop();
+        break;
+        default:
+            throw Error(`Three or less arguments expected but ${argumentsArray.length} given`)
+    }
 
     if(!protocols[protocol])
         throw Error('Wrong protocol');
 
+    const item = this;
+
+    console.log(argumentsArray)
+
     return new Promise(
         (resolve, reject) => {
-        protocols[protocol].request(url, options, (res) => {
+        protocols[protocol].request(...argumentsArray, (res) => {
             let data = '';
 
             res
