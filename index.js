5e724691e34514d806300253349da2dfa819f0bc
Samuel <brosse.samuel91@gmail.com>
2024-01-11 11:38:39 +0100

    Jaxa script is now a class with pre implemented https request method

--- index.js
+++ index.js
@@ -3,56 +3,143 @@ import http from "http";
 
 const protocols = { https, http };
 
-export function Jaxa(){
-
-    let argumentsArray = Object.values(arguments)
-    let callbacks = {};
-    const protocol = typeof argumentsArray[0] == "string" ? 
-        new URL(argumentsArray[0]).protocol.slice(0, -1) :
-        argumentsArray[0].protocol.slice(0, -1);
-
-    switch(argumentsArray.length){
-        case 1: 
-            if(typeof argumentsArray[0] == "string")
-                argumentsArray.push({method: 'GET'})
-        break;
-        case 2:
-            const objectKeys = Object.keys(argumentsArray[1])
-            if(['data', 'end', 'error'].filter((key) => objectKeys.includes(key)).length > 0)
-                callbacks = argumentsArray.pop();
-        break;
-        case 3:
-            callbacks = argumentsArray.pop();
-        break;
-        default:
-            throw Error(`Three or less arguments expected but ${argumentsArray.length} given`)
+export default class Jaxa{
+    constructor(){
+        
     }
 
-    if(!protocols[protocol])
-        throw Error('Wrong protocol');
+    request(){
+        const { requestArguments, callbacks } = surchage(...arguments);
 
-    const item = this;
-
-    return new Promise(
-        (resolve, reject) => {
-        protocols[protocol].request(...argumentsArray, (res) => {
-            let data = '';
-
-            res
-            .on('data', (chunk) => {
-                data += callbacks.data ? callbacks.data.apply(item, [chunk]) : chunk;
+        const protocol = typeof requestArguments[0] == "string" ? 
+            new URL(requestArguments[0]).protocol.slice(0, -1) :
+            requestArguments[0].protocol.slice(0, -1);
+    
+        if(!protocols[protocol])
+            throw Error('Wrong protocol');
+    
+        const item = this;
+    
+        return new Promise(
+            (resolve, reject) => {
+            protocols[protocol].request(...requestArguments, (res) => {
+                let data = '';
+    
+                res
+                .on('data', (chunk) => {
+                    data += callbacks.data ? callbacks.data.apply(item, [chunk]) : chunk;
+                })
+                .on('end', () => {
+                    resolve(callbacks.end ? callbacks.end.apply(item, [data]) : data);
+                })
+                .on('error', (err) => {
+                    reject(callbacks.error ? callbacks.error.apply(item, [err]) : err);
+                })
+    
             })
-            .on('end', () => {
-                resolve(callbacks.end ? callbacks.end.apply(item, [data]) : data);
+            .on("error", (err) => {
+                console.log(err)
             })
-            .on('error', (err) => {
-                reject(callbacks.error ? callbacks.error.apply(item, [err]) : err);
-            })
+            .end();
+        });
+    }
 
-        })
-        .on("error", (err) => {
-            console.log(err)
-        })
-        .end();
-    });
+    get(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'GET';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    post(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'POST';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    put(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'PUT';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    delete(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'DELETE';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    connect(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'CONNECT';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    options(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'OPTIONS';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    trace(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'TRACE';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    patch(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'PATCH';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+    head(){
+        const { requestArguments, callbacks } = surchage(...arguments);
+
+        requestArguments[requestArguments.length-1].method = 'HEAD';
+
+        return this.request(...requestArguments, callbacks);
+    }
+
+}
+
+function surchage(){
+    let surchageArguments = {
+        requestArguments: Object.values(arguments),
+        callbacks: {}
+    }
+
+    switch(surchageArguments.requestArguments.length){
+        case 1: 
+            if(typeof surchageArguments.requestArguments[0] == "string")
+                surchageArguments.requestArguments.push({method: 'GET'})
+        break;
+        case 2:
+            const objectKeys = Object.keys(surchageArguments.requestArguments[1])
+            if(['data', 'end', 'error'].filter((key) => objectKeys.includes(key)).length > 0 || objectKeys.length == 0)
+                surchageArguments.callbacks = surchageArguments.requestArguments.pop();
+        break;
+        case 3:
+            surchageArguments.callbacks = surchageArguments.requestArguments.pop();
+        break;
+        default:
+            throw Error(`Three or less arguments expected but ${surchageArguments.requestArguments.length} given`)
+    }
+
+    return surchageArguments
 }
