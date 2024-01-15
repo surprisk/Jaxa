6428ba958352d2905ea4b6888537c4caf8c83ba4
Samuel <brosse.samuel91@gmail.com>
2024-01-15 08:36:48 +0100

    Class option in progress

--- index.js
+++ index.js
@@ -5,17 +5,39 @@ const protocols = { https, http };
 
 export default class Jaxa{
     constructor(){
+        const argumentsArray = Object.values(arguments)
         
+        this.default = {};
+
+        switch(argumentsArray.length){
+            case 0:
+                break;
+            case 1:
+                const objectKeys = Object.keys(argumentsArray[0])
+                if(['data', 'end', 'error', 'callback'].filter((key) => objectKeys.includes(key)).length > 0 || objectKeys.length == 0)
+                    this.default.callbacks = argumentsArray[0];
+                else
+                    this.default.options = argumentsArray[0];
+                break;
+            case 2:
+                this.default.options = argumentsArray[0];
+                this.default.callback = argumentsArray[1];
+                break;
+            default:
+                throw Error(`Two or less arguments expected but ${argumentsArray.length} given`)
+        }
+            
     }
 
     request(){
         const item = this;
         const { requestArguments, callbacks } = surchage(...arguments);
+
         const options = getLastArgument(requestArguments);
 
         const protocol = typeof requestArguments[0] == "string" ? 
             new URL(requestArguments[0]).protocol.slice(0, -1) :
-            requestArguments[0].protocol.slice(0, -1);
+            options.protocol.slice(0, -1);
     
         if(!protocols[protocol])
             throw Error('Wrong protocol');
@@ -23,16 +45,16 @@ export default class Jaxa{
         return new Promise(
             (resolve, reject) => {
             const req = protocols[protocol].request(...requestArguments, callbacks.callback ? (res) => callbacks.callback.apply(item, [res, resolve, reject])  : (res) => {
-                let data = '';
+                let data = [];
     
                 res
                 .on('data', (chunk) => {
                     callbacks.data ?
                         callbacks.data.apply(item, [chunk, data]) :
-                        data += chunk;
+                        data.push(chunk);
                 })
                 .on('end', () => {
-                    resolve(callbacks.end ? callbacks.end.apply(item, [data]) : data);
+                    resolve(callbacks.end ? callbacks.end.apply(item, [data]) : Buffer.concat(data));
                 })
                 .on('error', (err) => {
                     reject(callbacks.error ? callbacks.error.apply(item, [err]) : err);
@@ -128,17 +150,15 @@ function surchage(){
 
     switch(surchageArguments.requestArguments.length){
         case 1: 
-            if(typeof surchageArguments.requestArguments[0] == "string")
-                surchageArguments.requestArguments.push({method: 'GET'})
-        break;
+            break;
         case 2:
             const objectKeys = Object.keys(surchageArguments.requestArguments[1])
             if(['data', 'end', 'error', 'callback'].filter((key) => objectKeys.includes(key)).length > 0 || objectKeys.length == 0)
                 surchageArguments.callbacks = surchageArguments.requestArguments.pop();
-        break;
+            break;
         case 3:
             surchageArguments.callbacks = surchageArguments.requestArguments.pop();
-        break;
+            break;
         default:
             throw Error(`Three or less arguments expected but ${surchageArguments.requestArguments.length} given`)
     }
